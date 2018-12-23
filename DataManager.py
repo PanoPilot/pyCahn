910086eaecda9dfef892d4a65b0df3d104469cd7
PanoPilot <chris.kletti@gmx.de>
2018-12-23 22:28:54 +0100

    cla parser, json configs, yahoo_fin package

--- DataManager.py
+++ DataManager.py
@@ -12,30 +12,99 @@
 from Exchanges.Exchange import Exchange
 from Exchanges.Kraken_Ex import Kraken_Ex
 
+import json
+from yahoo_fin.stock_info import *
+
+I_TYPE_Y = "YF"
+I_TYPE_W = "WEB"
+I_TYPE_E = "EXHANGE"
+
 class DataManager():
 
-    def __init__(self, exchange, asset, currency):
+    def __init__(self, i_config_f, a_config_f):
 
-        #HERE: add a check of suppored exchanges assets etc!
+        #first read config files
+        with open(i_config_f, 'r') as f:
+            self.i_config = json.load(f)
 
+        with open(a_config_f, 'r') as f:
+            self.a_config = json.load(f) 
+ 
+        #print (self.i_config["IMPORT"]["FROM"])
         #print (exchange, asset, currency)
-        self.exchange_name = exchange
-        self.asset = asset
-        self.currency = currency
-
+        #self.exchange_name = exchange
+        #self.asset = asset
+        #self.currency = currency
         #init exchange
         #REMARK: Switch req. later based on different exchanges 
-        self.exchange = Kraken_Ex()
+        #self.exchange = Kraken_Ex()
 
- 
+
+
+    #######################################################################
+    # 
+    # Method to start importing data based on different types
+    # Types:
+    #   1) WEB:         read data from web/url source
+    #   2) yahoo_fin:   usage of yahoo_fin package
+    #   3) exchange:    Read data from a exchange API
+    #
+    # Returns:
+    #   
+    # 
+    ######################################################################## 
+    def start_import(self):
+
+        #get assets to import
+        symbols = self.a_config["ASSETS"]["SYMBOLS"] 
+
+        #check import source
+        if (self.i_config["IMPORT"]["TYPE"] == I_TYPE_Y):
+            #ok in this case, use yahoo_fin package
+            df = self.yf_import_data_ohlcv(symbols)
+        elif(self.i_config["IMPORT"]["TYPE"] == I_TYPE_W):
+            pass
+        elif(self.i_config["IMPORT"]["TYPE"] == I_TYPE_E):
+            pass
+    
+        #print (df)
+
+
+
+
+    #######################################################################
+    # 
+    # Method to import data by using yahoo_fin package
+    # Parameters:
+    #   symbols: symbols of securities to be imported
+    #
+    # Returns:   
+    #   Pandas Dataframe with imported data
+    # 
+    ######################################################################## 
+    def yf_import_data_ohlcv(self, symbols):
+
+        #symbols = symbols[:3]
+        data = pd.DataFrame([])
+
+        for sym in symbols:
+            print('Get Data for:', sym["SYMBOL"])
+            data = data.append(get_data(sym["SYMBOL"],index_as_date=False))
+       
+
+        print(data)
+        return data
+
+
+
 
     def import_ohlcv(self, from_date, to_date):
-        print ('Importing:', self.exchange_name, self.currency, self.asset, 'From:', from_date, 'to', to_date)
+        #print ('Importing:', self.exchange_name, self.currency, self.asset, 'From:', from_date, 'to', to_date)
 
         #self.exchange.getHistory_OHLCV(self.asset,self.currency,from_date)
 
         ##self.exchange.import_ohlcv(self.asset,self.currency,from_date,to_date)
-        self.exchange.getHistory_Trades(self.asset,self.currency,from_date,to_date)
-        
+        #self.exchange.getHistory_Trades(self.asset,self.currency,from_date,to_date)
+        pass
 
 
--- pyCahn.py
+++ pyCahn.py
@@ -5,23 +5,131 @@
 #import subprocess
 #import timeit
 #import os
-#import sys
+import sys
+import getopt
 
 import datetime
+import json
 
 # Define Custom imports
 from DataManager import DataManager
 
 
+MODE_TEST = 1
+MODE_IMPORT = 2
+MODE_WATCH = 3
+MODE_TRADE = 4
+
+#######################################################################
+# 
+# Method to check and parse Command Line Argurments (cla)
+# Parameters:
+#   argv : command line arquements
+#
+# Returns:
+#   1) mode = <MODE_TEST | MODE_IMPORT | MODE_WATCH |MODE_TRADE>
+#   2) name of mode config file
+#   3) name of asset config file
+# 
+######################################################################## 
+def check_cla(argv):
+    #  -backtest: -import: -watch: -trade: -asset:
+    mode = 0
+    mode_config = ""
+    asset_config = ""
+
+    try:
+        opts, args = getopt.getopt(argv,"hb:i:w:t:a:", ["backtest=", "import=", "watch=", "trade=", "asset="])
+    except getopt.GetoptError as err:
+        print(err)
+        print('pyCahn.py [-backtest=config-file -import=config-file -watch=config-file -trade=config-file] -asset=config-file')
+        print('pyCahn.py [-b <config-file> -i <config-file> -w <config-file> -t <config-file>] -asset=config-file')
+        print('Only one mode possible [-backtest -import -watch -trade]')
+        sys.exit(2)
+      
+    for opt, arg in opts:
+        if opt == '-h':
+            print('pyCahn.py [-backtest=config-file -import=config-file -watch=config-file -trade=config-file] -asset=config-file')
+            print('pyCahn.py [-b <config-file> -i <config-file> -w <config-file> -t <config-file>] -asset=config-file')
+            print('Only one mode possible [-backtest -import -watch -trade]')
+            sys.exit()
+        elif opt in ("-b", "--backtest"):
+            if mode!=0:
+                print('ERROR: Only one mode possible [-backtest -import -watch -trade]')
+                sys.exit() 
+            mode_config = arg
+            mode = MODE_TEST
+        elif opt in ("-i", "--import"):
+            if mode!=0:
+                print('ERROR: Only one mode possible [-backtest -import -watch -trade]')
+                sys.exit() 
+            mode_config = arg
+            mode = MODE_IMPORT
+        elif opt in ("-w", "--watch"):
+            if mode!=0:
+                print('ERROR: Only one mode possible [-backtest -import -watch -trade]')
+                sys.exit() 
+            mode_config = arg
+            mode = MODE_WATCH
+        elif opt in ("-t", "--trade"):
+            if mode!=0:
+                print('ERROR: Only one mode possible [-backtest -import -watch -trade]')
+                sys.exit() 
+            mode_config = arg
+            mode = MODE_TRADE
+        elif opt in ("-a", "--asset"):
+            asset_config = arg
+
+    if (mode_config == "" or asset_config == ""):
+        print('ERROR: No mode or asset config file provided')
+        sys.exit()
+
+    return mode, mode_config, asset_config
+
+
+#######################################################################
+# 
+# Method to handle import of data and stores it to hdf file
+# Parameters:
+#   i_config: name of mode config file
+#   a_config: name of asset config file
+#
+# Returns:
+#   
+# 
+######################################################################## 
+def handle_import(i_config, a_config):
+    #init Datamanager
+    dm = DataManager(i_config, a_config)
+    
+    #start import
+    dm.start_import()
+
+#######################################################################
+# 
+# Main method 
+#
+# Supported Modes:
+#   1) Backtesting of strategies
+#   2) Importing of data
+#   3) Realtime watching and updating of asset data
+#   4) Realtime trading of assets with given strategie
+#   
+######################################################################## 
 if __name__ == "__main__":
     
-    data_manager = DataManager("Kraken", "ETH", "EUR")
-    
-    #print(data_manager.asset, data_manager.exchange_name, data_manager.currency)
+    # we need an argument parser at this place
+    mode, mode_config, asset_config = check_cla(sys.argv[1:])
 
-    from_time = datetime.datetime(2018, 1, 1)
-    to_time = datetime.datetime.now()
-
-    data_manager.import_ohlcv(from_time, to_time)
-
-    #print (from_time, '-->', to_time)
+    # Check mode
+    if mode == MODE_IMPORT:
+        handle_import(mode_config, asset_config)    
+    elif mode == MODE_TEST:
+        pass
+    elif mode == MODE_TRADE:
+        pass
+    elif mode == MODE_TRADE:
+        pass
+    #print(mode, mode_config, asset_config)
+    #data_manager_krak = DataManager("Kraken", "ETH", "EUR")
+    #data_manager_krak.import_ohlcv(from_time, to_time)
